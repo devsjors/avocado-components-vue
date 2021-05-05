032de2ed4b90b7be77ee44c5b58c85a0536c2699
Sjors Eveleens <sjors@MacBook-Pro-van-Sjors.local>
2021-05-05 20:29:10 +0200

    Global Mixin added

--- src/main.js
+++ src/main.js
@@ -3,8 +3,18 @@ import App from "./App.vue";
 import router from "./router";
 import "@/styles/reset.css/";
 
+import DefaultTheme from "@/theme/DefaultTheme";
+const theme = {
+  computed: {
+    __theme() {
+      return DefaultTheme;
+    },
+  },
+};
+
 Vue.config.productionTip = false;
 
+Vue.mixin(theme);
 new Vue({
   router,
   render: (h) => h(App),
